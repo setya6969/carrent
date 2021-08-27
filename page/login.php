ea7c9547829c5bad5e00954ef63fb49286941f74
Zaka Noor <zakanoor@outlook.co.id>
2021-08-27 10:30:14 +0700

    add attribute name on input and change input email to text

--- page/login.php
+++ page/login.php
@@ -15,18 +15,18 @@
     <div class="">
         <div class="error" style="display: none"></div>
         <div class="login-dark">
-            <form class="form">
+            <form class="form" action="../function/f_login.php" method="post">
                 <h2 class="sr-only">Login Form</h2>
                 <div class="illustration"><i class="icon ion-ios-locked-outline"></i></div>
-                <div class="form-group input-field"><input class="form-control" type="email" id="email" placeholder="Email"></div>
-                <div class="form-group input-field"><input class="form-control" type="password" id="password" placeholder="Password"></div>
-                <div class="form-group"><input class="btn btn-primary btn-block" type="submit" value="Masuk"></input></div>
+                <div class="form-group input-field"><input class="form-control" type="text" id="email" placeholder="Email" name="username"></div>
+                <div class="form-group input-field"><input class="form-control" type="password" id="password" placeholder="Password" name="password"></div>
+                <div class="form-group"><input class="btn btn-primary btn-block" type="submit" value="Masuk" name="login"></input></div>
                 <a href="register.php" class="forgot">Register here!</a>
             </form>
         </div>
     </div>
     
-    <script src="../js/app.js"></script>
+    <!-- <script src="../js/app.js"></script> -->
 
 </body>
 
